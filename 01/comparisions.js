1dc38c98916a122fd00ff98c952087a9ad21f33b
anshikaa05 <66886152+anshikaa05@users.noreply.github.com>
2025-03-10 19:31:36 +0000

    config

---
+++ 01/comparisions.js
@@ -0,0 +1,14 @@
+console.log("2"===2);
+console.log(2==2);
+
+// console.log(null>=0); null is converted to 0 so thus ut comes to be true
+console.log(null<0);
+console.log(null==0);
+
+console.log(undefined == 0);
+console.log(undefined>0)
+console.log(undefined<0)
+
+// Summary that we could take is difference between == and ===
+== any type of type of datatype is verified
+=== a bit strict only for simlar type of datatype comparisions could happen
