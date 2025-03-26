fab22008961c6f7e9f7b8225690fe6895185d160
anshikaa05 <66886152+anshikaa05@users.noreply.github.com>
2025-03-26 20:40:31 +0000

    project6

--- 05_projects/project6.js
+++ 05_projects/project6.js
@@ -6,3 +6,19 @@ const randomColor = function () {
     }
     return color;
 };
+let intervalid;
+const actiononstart = function () {
+    if (!intervalid) {
+        intervalid = setInterval(changeBgcolor, 2000);
+    }
+};
+function changeBgcolor() {
+    document.body.style.backgroundColor = randomColor();
+}
+const actiononstop = function () {
+    clearInterval(intervalid);
+    intervalid = null;
+};
+document.querySelector('#start').addEventListener('click', actiononstart);
+document.querySelector('#stop').addEventListener('click', actiononstop);
+document.querySelector('#stop').addEventListener('click', actiononstop);
