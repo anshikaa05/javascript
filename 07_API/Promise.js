49c4b16dca4de08637d838281554d8c705abc216
anshikaa05 <66886152+anshikaa05@users.noreply.github.com>
2025-03-27 21:25:32 +0000

    promise

--- 07_API/Promise.js
+++ 07_API/Promise.js
@@ -1,8 +1,58 @@
-const promiseone = new Promise(function (resolve, reject) {
-    setTimeout(() => {
-        console.log("Async task 1");
+// const promiseone = new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//         console.log("Async task 1");
+//         resolve();
+//     }, 1000);
+// }).then(function () {
+//     console.log("Resolved");
+// });
+
+// Q or Bluebird was used before Promises
+
+const promiseOne = new Promise(function (resolve, reject) {
+    // Do an async task
+    // DB calls, cryptography,network
+    setTimeout(function () {
+        console.log('Async task is completed');
         resolve();
-    }, 1000);
+    }, 1000)
+
+})
+
+promiseOne.then(function () {
+    console.log("Promise consumed");
+})
+// .then directly connection with resolve
+
+new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        console.log('Async task 2 is completed');
+        resolve();
+    }, 1000)
 }).then(function () {
-    console.log("Resolved");
-});
+    console.log("Promise 2 consumed");
+})
+
+const promiseThree = new Promise(function (resolve, reject) {
+
+    setTimeout(function () {
+        resolve({ username: "Anshika", email: "canshsu@gmail.com" });
+    }, 1000)
+
+})
+
+promiseFour.then(function (user) {
+    console.log(user);
+})
+
+const promiseFour = new Promise(function (resolve, reject) {
+
+    setTimeout(function () {
+        resolve({ username: "Anshika", email: "canshsu@gmail.com" });
+    }, 1000)
+
+})
+
+promiseThree.then(function (user) {
+    console.log(user);
+})
