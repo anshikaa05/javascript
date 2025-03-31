c110b17c1f8180422d02dc093c03376b111ad0a7
anshikaa05 <66886152+anshikaa05@users.noreply.github.com>
2025-03-31 15:57:40 +0000

    cc

---
+++ Arrays/array.js
@@ -0,0 +1,23 @@
+// for of
+
+// const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+// const greetings = "Hello World!";
+
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`);
+// }
+
+// Maps
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of Amreica")
+map.set('FR', "France")
+map.set('EUR', "Europe")
+
+console.log(map)
+
+// Maps contain only unique values
